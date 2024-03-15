b0754252f8c9c7e31996c47dc92767b09d85cba4
ADEP-123 <andep210399@gmail.com>
2024-03-15 12:21:20 -0500

    feat: :sparkles: Dibujando estructura general

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,65 @@ const white = "#ffffff"
 const butStartPause = document.querySelector(".startPauseButton");
 const butReset = document.querySelector(".resetButton");
 
+// Dibujar primer circulo externo
+context.beginPath();
+context.fillStyle = darkRed
+context.arc(radio, radio, radio, 0, 2 * Math.PI);
+context.fill();
+
+context.beginPath();
+context.fillStyle = black
+context.arc(radio, radio, radio - 5, 0, 2 * Math.PI);
+context.fill();
+
+//Dibujar segundo circulo externo
+context.beginPath();
+context.fillStyle = darkRed
+context.arc(radio, radio, radio - 8, 0, 2 * Math.PI);
+context.fill();
+
+context.beginPath();
+context.fillStyle = black
+context.arc(radio, radio, radio - 10, 0, 2 * Math.PI);
+context.fill();
+
+//Dibujar eje interno
+context.beginPath();
+context.fillStyle = white
+context.arc(radio, radio, 10, 0, 2 * Math.PI);
+context.fill();
+
+//Dibujar numeros e indicadores
+context.font = radio / 10 + "px arial";
+context.textAlign = "center";
+context.textBaseline = "middle";
+for (let i = 0; i < 60; i++) {
+    context.strokeStyle = white;
+    //Calcular coordenadas de inicio de linea de segundos
+    let x2 = radio + (radio - 12) * Math.sin(i * 2 * Math.PI / 60);
+    let y2 = radio - (radio - 12) * Math.cos(i * 2 * Math.PI / 60);
+    let x1 = 0;
+    let y1 = 0;
+    if (i % 5 == 0) {
+        //Dibujar numeros cada 5 segundos
+        context.fillText(i, radio + (radio - 30) * 0.9 * Math.sin(i * 2 * Math.PI / 60), radio - ((radio - 30) * 0.9 * Math.cos(i * 2 * Math.PI / 60)));
+
+        // Calcular coordenadas de final de linea de segundos multiplos de 5
+        context.lineWidth = 3;
+        x1 = radio + (radio - 27) * Math.sin(i * 2 * Math.PI / 60);
+        y1 = radio - (radio - 27) * Math.cos(i * 2 * Math.PI / 60);
+    } else {
+        // Calcular coordenadas de final de linea de otros segundos
+        context.lineWidth = 2;
+        y1 = radio - (radio - 22) * Math.cos(i * 2 * Math.PI / 60);
+        x1 = radio + (radio - 22) * Math.sin(i * 2 * Math.PI / 60);
+    }
+    // Dibujar las lineas de los segundos
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+};
+
+
+
